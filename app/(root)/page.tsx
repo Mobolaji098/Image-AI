28c92e13b36fc61f98d048256ba739009141d99d
Barterway <barterwayapp.gmail.com>
2024-10-06 01:30:21 +0100

    intial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,14 @@
+import { SignOutButton,UserButton } from "@clerk/nextjs";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      {/* <p>Home</p>
+      <SignOutButton/> 
+      <UserButton />  */}
+    </div>
+  );
+};
+
+export default Home;
