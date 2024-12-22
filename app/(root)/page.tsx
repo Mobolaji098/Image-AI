088e7499c8bac2a46e634b4d85bacde390aa7fd5
Barterway <barterwayapp.gmail.com>
2024-12-22 16:39:47 +0100

    full

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,46 @@
 // import { SignOutButton,UserButton } from "@clerk/nextjs";
+import { Collection } from "@/components/shared/Collection";
+import { navLinks } from "@/constants";
+import { getAllImages } from "@/lib/actions/image.actions";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
-const Home = () => {
+const Home = async ({ searchParams }: SearchParamProps) => {
+  const page = Number(searchParams?.page) || 1;
+  const searchQuery = (searchParams?.query as string) || "";
+  const images = await getAllImages({ page, searchQuery });
   return (
-    <div>
-      {/* <p>Home</p>
-      <SignOutButton/> 
-      <UserButton />  */}
-    </div>
+    <>
+      <section className="home">
+        <h1 className="home-heading">
+          Unleash your creative vision with Imagnify
+        </h1>
+        <ul className="flex-center w-full gap-20">
+          {navLinks.slice(1, 5).map((link) => (
+            <Link
+              key={link.route}
+              href={link.route}
+              className="flex-center gap-2 flex-col"
+            >
+              <li className="p-4 rounded-full bg-white w-fit flex-center">
+                <Image src={link.icon} alt="Image" height={24} width={24} />
+              </li>
+              <p className="text-white p-14-medium text-center">{link.label}</p>
+            </Link>
+          ))}
+        </ul>
+      </section>
+
+      <section className="sm:mt-12">
+        <Collection
+          hasSearch={true}
+          images={images?.data}
+          totalPages={images?.totalPage}
+          page={page}
+        />
+      </section>
+    </>
   );
 };
 
